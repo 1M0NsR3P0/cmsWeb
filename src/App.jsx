95f2d260a23970aabcded4f1a8e33e6ae464affb
1M0NsR3P0 <naimonchy@gmail.com>
2023-08-04 11:46:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  // cms system with mongodb
+
+
+  return (
+    <div>
+        <div>
+          this is the nav
+        </div>
+        <div>
+          this is the nav content
+        </div>
+    </div>
+  )
+}
+
+export default App
